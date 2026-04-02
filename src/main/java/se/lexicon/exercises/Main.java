2a8a366da59386b86d1dc9cc078d2ade71c427f8
Maja <maja.bloom@outlook.com>
2026-04-02 10:41:38 +0200

    Add part 2 exercises, make final adjustments to Main

--- src/main/java/se/lexicon/exercises/Main.java
+++ src/main/java/se/lexicon/exercises/Main.java
@@ -34,8 +34,13 @@ import java.util.function.Consumer;
                 List<Person> inactivePeople = filterPeople(people, notActive);
                 processPeople(inactivePeople, printName);
 
+                System.out.println("\n--- Testing with Predicate ---");
+                List<Person> adultsWithPredicate = filterWithPredicate(people, p -> p.getAge() >= 18);
+                processPeople(adultsWithPredicate, p -> System.out.println(p.getName() + " is an adult"));
+
                 // Part 2:
 
+                System.out.println();
                 System.out.println("\n--- PART 2 ---");
 
                 // Filter all active
@@ -48,13 +53,13 @@ import java.util.function.Consumer;
                 List<String> names = people.stream()
                         .map(p -> p.getName())
                         .toList();
-                System.out.println("All names: " + names);
+                System.out.println("\nAll names: " + names);
 
                 // Count adults
                 long adultCount = people.stream()
                         .filter(p -> p.getAge() >= 18)
                         .count();
-                System.out.println("Amont of adults: " + adultCount);
+                System.out.println("\nAmount of adults: " + adultCount);
 
                 // Get unique cities
                 List<String> cities = people.stream()
@@ -62,8 +67,30 @@ import java.util.function.Consumer;
                         .distinct()
                         .sorted()
                         .toList();
-                System.out.println("Cities in the list: " + cities);
+                System.out.println("\nCities in the list: " + cities);
 
+                // Sort by age
+                List<Person> sortedByAge = people.stream()
+                        .sorted((p1, p2) -> Integer.compare(p1.getAge(), p2.getAge()))
+                        .toList();
+                System.out.println("\nPeople sorted by age: ");
+                sortedByAge.forEach(p -> System.out.println(" * " + p.getName() + " (" + p.getAge() + " years)"));
+                System.out.println();
+
+                // Find first active person in Stockholm
+                // Use .ifPresent to only print IF I find someone.
+                people.stream()
+                        .filter(p -> p.isActive() && p.getCity().equals("Stockholm"))
+                        .findFirst()
+                        .ifPresent(p -> System.out.println("\nFirst active in Stockholm: " + p.getName()));
+
+                // Mapping + Formatting
+                List<String> formattedInfo = people.stream()
+                        .map(p -> p.getName() + " (" + p.getAge() + ") - " + p.getCity())
+                        .toList();
+
+                System.out.println("\nFormatted info list: ");
+                formattedInfo.forEach(line -> System.out.println(" - " + line));
             }
 
             // --- REUSABLE METHODS ---
